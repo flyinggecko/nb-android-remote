93ab3f636f9eb138e045708bfe67b1e080e960f8
Julian Stiller <julian@flyinggecko.org>
2013-06-21 22:08:20 +0200

    inital start of the app. first activity in progress

---
+++ src/org/flyinggecko/nb_android_remote/Startscreen.java
@@ -0,0 +1,25 @@
+package org.flyinggecko.nb_android_remote;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Startscreen extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_startscreen);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.startscreen, menu);
+		return true;
+	}
+
+}
